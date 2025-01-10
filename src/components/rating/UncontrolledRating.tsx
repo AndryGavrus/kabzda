d3e705a0720ea4c083a81e04a0a62c0495e4e2a2
Andrey <andry.gavrus@gmail.com>
2025-01-10 17:56:53 +0300

    Made 6 task

--- src/components/rating/UncontrolledRating.tsx
+++ src/components/rating/UncontrolledRating.tsx
@@ -8,11 +8,11 @@ export const UncontrolledRating = (props: UncontrolledRatingPropsType) => {
   let [value, setValue] = useState(0)
   return (
     <div>
-      <Star selected={value > 0} /> <button>*</button>
-      <Star selected={value > 1} /> <button>*</button>
-      <Star selected={value > 2} /> <button>*</button>
-      <Star selected={value > 3} /> <button>*</button>
-      <Star selected={value > 4} /> <button>*</button>
+      <Star selected={value > 0} /> <button onClick={()=>{setValue(1)}}>*</button>
+      <Star selected={value > 1} /> <button onClick={()=>{setValue(2)}}>*</button>
+      <Star selected={value > 2} /> <button onClick={()=>{setValue(3)}}>*</button>
+      <Star selected={value > 3} /> <button onClick={()=>{setValue(4)}}>*</button>
+      <Star selected={value > 4} /> <button onClick={()=>{setValue(5)}}>*</button>
     </div>
   )
 };
